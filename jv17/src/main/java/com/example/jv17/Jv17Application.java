255eba946bce0fa7d7c0cfc1e82561b5e0f09560
Julio César Barbosa da Silva <j.barbosa.da.silva@avanade.com>
2024-11-07 09:34:37 -0300

    Add project

---
+++ jv17/src/main/java/com/example/jv17/Jv17Application.java
@@ -0,0 +1,13 @@
+package com.example.jv17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jv17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jv17Application.class, args);
+	}
+
+}
