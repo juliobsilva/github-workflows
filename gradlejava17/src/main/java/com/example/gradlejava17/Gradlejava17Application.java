d6650ed431bcddaa9a34eaa46f2ccffc47d9bca1
Julio César Barbosa da Silva <j.barbosa.da.silva@avanade.com>
2024-11-07 12:58:25 -0300

    Add project

---
+++ gradlejava17/src/main/java/com/example/gradlejava17/Gradlejava17Application.java
@@ -0,0 +1,13 @@
+package com.example.gradlejava17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gradlejava17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gradlejava17Application.class, args);
+	}
+
+}
---
+++ gradlejava17/src/test/java/com/example/gradlejava17/Gradlejava17ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gradlejava17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gradlejava17ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
